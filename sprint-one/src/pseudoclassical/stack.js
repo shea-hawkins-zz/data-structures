a70d442ae608ef4f335f2f01dd81254b14bc6f4f
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 16:25:14 -0700

    Implement size on Stack.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._length = 0;
+  this._storage = {};
 };
 
-
+Stack.prototype.size = function(value) {
+  return this._length;
+};
