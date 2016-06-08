ee9833f845119f0bfb91ce1cf03183a6b2adc80b
Shea Hawkins <Shea Hawkins>
2016-06-08 13:14:32 -0700

    Implement size on functional-shared Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,19 @@
-var Queue = function() {
+var Queue = function(...args) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.length = 0;
+  Object.keys(queueMethods).forEach(function(key) {
+    queue[key] = queueMethods[key];
+  });
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: () => {
+    return length;
+  }
+};
 
-
+Queue.prototype = queueMethods;
