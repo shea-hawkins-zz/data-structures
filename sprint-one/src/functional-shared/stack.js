5790f4c4e46779cfdaf27ed012f40f09d4500456
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 15:35:14 -0700

    'Implement size'

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.length = 0;
+  stack.storage = {};
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.length;
+  }
+};
 
 
