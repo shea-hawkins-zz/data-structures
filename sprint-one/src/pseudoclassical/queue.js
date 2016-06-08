9a4e19ee593a483399184ad4d49c5c2a0ce03fbb
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 16:33:20 -0700

    Implement dequeue on Queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -10,3 +10,16 @@ Queue.prototype.size = function() {
   return this.tail - this.head;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.tail] = value;
+  this.tail++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.tail > this.head) {
+    var dequeuedValue = this.storage[this.head];
+    this.storage[this.head] = undefined;
+    this.head++;
+    return dequeuedValue;
+  }
+}
