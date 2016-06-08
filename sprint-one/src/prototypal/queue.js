2e894a765ef3e8714a4350ce7dc28f70e49bd88c
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 16:05:19 -0700

    'Implement size on Stack'

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(Queue.prototype);
+  queue.head = 0;
+  queue.tail = 0;
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.tail - this.head;
+  }
+};
+
+Queue.prototype = queueMethods;
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.length = 0;
+  stack.storage = {};
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.length;
+  }
+};
+
+
 
 
