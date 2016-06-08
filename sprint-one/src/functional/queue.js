0cd86044f2995cfc83dc87219f5162ae8c721aed
Shea Hawkins <Shea Hawkins>
2016-06-08 12:07:15 -0700

    Implement private shift on Queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,8 +5,15 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var shift = function () {
+    for (var i = length - 1; i >= 0; i--) {
+      storage[i + 1] = storage[i];
+    }
+    storage[0] = undefined;
+  };
   someInstance.enqueue = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
