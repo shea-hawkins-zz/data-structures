032d9f7e39db8f0ec8017994e863f041b335c276
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 10:33:26 -0700

    Implement addNode, contains on Graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,14 +2,22 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  var graph = Object.create(Graph.prototype);
+  graph.nodes = [];
+  return graph;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var node = GraphNode(node);
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.some(this.nodes, function(childNode) {
+    return childNode.value === node;
+  });
 };
 
 // Removes a node from the graph.
@@ -30,6 +38,13 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+};
+
+var GraphNode = function(value) {
+  var node = {};
+  node.neighbors = [];
+  node.value = value;
+  return node;
 };
 
 /*
