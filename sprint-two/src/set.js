e275eca2ab87209c37efaf8efbc52ec328a8c31e
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 11:12:13 -0700

    Implement add, contains to Set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,17 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
