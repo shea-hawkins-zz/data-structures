1ca17be19be8a9c6b272ad8e16a7dc16a310b261
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 11:04:31 -0700

    Implement forEachNode on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -41,6 +41,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.nodes) {
+    cb(node);
+  }
 };
 
 
