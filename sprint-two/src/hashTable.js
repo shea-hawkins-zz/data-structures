2a876a4ef5d15d8aac08facf63d97efeac1ce5ad
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 13:51:04 -0700

    Change hashtable implementation to use [key, value] arrays at keys.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,42 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._conflicts = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var node = {
+    key: k,
+    value: v
+  };
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [node]);
+  } else {
+    this._storage.get(index).push(node);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var node;
+  var nodes = this._storage.get(index).forEach(function(child) {
+    if (child.key === k) {
+      node = child;
+    }
+  });
+  return node ? node.value : undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var hashArray = this._storage.get(index);
+  if (hashArray) {
+    this._storage.set(index, hashArray.filter(function(node) {
+      return node.key !== k;
+    })); 
+  }
 };
 
 
