1961b297e2812e1d5db2b56b13bf7d8bf42a3a59
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 09:59:34 -0700

    Implement addChild to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,21 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
