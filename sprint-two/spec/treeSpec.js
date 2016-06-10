0186a4a6717f0b6546bdf8f9315397f8a8535d64
Hack Reactor Students <accounts@hackreactor.com>
2016-06-10 15:35:26 -0700

    Update tests.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -65,4 +65,19 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should run the traversal function on each child', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    var results = [];
+    var callback = function(item) {
+      results.push(item);
+    };
+    tree.traverse(callback);
+
+    expect(results).to.deep.equal([5, 7, 6, 8]);
+    expect(tree.contains(8)).to.deep.equal(true);
+  });
+
 });
